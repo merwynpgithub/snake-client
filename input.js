0c7a64e56bb426bb2cd5812ec778b13abde2833e
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-17 16:54:58 -0500

    added wasd movement controls to input

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
 // setup interface to handle user input from stdin
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,6 +14,18 @@ const handleUserInput = function (key) {
   // \u0003 maps to ctrl+c input
 if (key === '\u0003') {
   process.exit();
+}
+if (key === 'w') {
+  connection.write("Move: up");
+}
+if (key === 'a') {
+  connection.write("Move: left");
+}
+if (key === 's') {
+  connection.write("Move: down");
+}
+if (key === 'd') {
+  connection.write("Move: right");
 }
 };
 module.exports = setupInput;
