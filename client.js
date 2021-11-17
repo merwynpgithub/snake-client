15584fd91ab0b7f6ebd873111815cdd160319285
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-17 11:39:52 -0500

    added client file and handled message data from the server

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (message) => {
+    console.log(`The server responded ${message}`);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,21 +1,6 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',// IP address here,
-    port: 50541// PORT number here,
-  });
+const connect = require('./client');
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (message) => {
-    console.log(`The server responded ${message}`);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
